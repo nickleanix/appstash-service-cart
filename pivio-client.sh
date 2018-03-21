b730da849c73afba3f4c75dc0633350270eba1c1
Dominik Rose <dominik.rose@leanix.net>
2018-03-21 13:22:43 +0100

    First pivio test

---
+++ pivio-client.sh
@@ -0,0 +1,3 @@
+export PIVIO_SERVICE_URL="https://app.leanix.net/services/integrations/v2/pivio/document"
+export PIVIO_API_TOKEN="kCUVmpkf5uxqenc7SZBymEa4e8uME52tdmnH665z"
+docker run --rm -v $(pwd):/source leanix/pivio-client pivio -serviceurl "${PIVIO_SERVICE_URL}" -verbose -addfield "api_token=${PIVIO_API_TOKEN}"
