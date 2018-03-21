104d1d132882462490d4bdfb90a6dc0565a6bc06
Dominik Rose <dominik.rose@leanix.net>
2018-03-21 13:09:15 +0100

    Initial maven

---
+++ src/main/java/net/leanix/appstash/service_cart/App.java
@@ -0,0 +1,13 @@
+package net.leanix.appstash.service_cart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
